40d0b7b2f0dc322d6303d47af7a21d0e62446508
SorinXV <sorinmartinescu06@gmail.com>
2024-02-09 00:36:46 +0100

    json data parsing

--- src/assets/pages/Obiettivi.js
+++ src/assets/pages/Obiettivi.js
@@ -1,7 +1,23 @@
-export default function Obiettivi(){
+// Obiettivi.js
+import React from 'react';
+import obiettiviData from '../../assets/data/obiettivi.json';
+
+const ObiettiviComponent = ({ obiettivoKey }) => {
+  const obiettivo = obiettiviData[obiettivoKey];
+    //This allert should be pointless be pointless cause if the path is wrong it automatically gets redirected to home
+  if (!obiettivo) {
+    return alert("L'obiettivo non è stato trovato.");
+  }
+
   return (
-  <>
-    Hello from Obiettivi
-  </>
+    <div>
+      Hello from 'Obiettivi'
+      <h1>{obiettivo.title}</h1>
+      <h2>{obiettivo.subtitle}</h2>
+      <p>{obiettivo.paragraph}</p>
+      {/* Render other properties as needed */}
+    </div>
   );
-}
+};
+
+export default ObiettiviComponent;
