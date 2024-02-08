fecea16b26945e8cf18c3b3c973de3e52aa8c347
SorinXV <sorinmartinescu06@gmail.com>
2024-02-09 00:47:41 +0100

    very minor changes

--- src/assets/pages/Obiettivi.js
+++ src/assets/pages/Obiettivi.js
@@ -4,14 +4,14 @@ import obiettiviData from '../../assets/data/obiettivi.json';
 
 const ObiettiviComponent = ({ obiettivoKey }) => {
   const obiettivo = obiettiviData[obiettivoKey];
-    //This allert should be pointless be pointless cause if the path is wrong it automatically gets redirected to home
+    //This alert should be pointless cause if the path is wrong it automatically gets redirected to home
+    // Edit: it wasn't pointless
   if (!obiettivo) {
-    return alert("L'obiettivo non è stato trovato.");
+    return alert("L'obiettivo non è stato trovato. Se leggi questa schermata scrivi a Sorin :) ");
   }
 
   return (
     <div>
-      Hello from 'Obiettivi'
       <h1>{obiettivo.title}</h1>
       <h2>{obiettivo.subtitle}</h2>
       <p>{obiettivo.paragraph}</p>
