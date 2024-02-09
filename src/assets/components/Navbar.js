7bb0f142d201186e2d37980b7f805d735fc33620
mater43 <bertimattia054@gmail.com>
2024-02-09 20:28:24 +0100

    change footer, navbar,home and css

--- src/assets/components/Navbar.js
+++ src/assets/components/Navbar.js
@@ -1,7 +1,17 @@
+import '../styles/header.css';
 export default function Navbar(){
   return (
-  <>
-    Hello from 'Navbar'
-  </>
+    <div className='header'>
+    <a href='/' classname='logo'>Agenda 2030</a>
+    <input type='checkbox' class='menuBtn' id='menuBtn' />
+    <label for='menuBtn' class='menuIcon'>
+      <span class='navIcon'></span>
+    </label>
+    <ul class='menu'>
+      <li><a href='/'>Home</a></li>
+      <li><a href='#'>Obiettivi</a></li>
+      <li><a href='/quiz'>Quiz</a></li>
+    </ul>
+    </div>
   );
 }
