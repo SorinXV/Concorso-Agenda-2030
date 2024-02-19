d2b4c2cf5cfa53518595e0e6066d150df9f4435a
Mattia Berto <125810753+BertoMVII@users.noreply.github.com>
2024-02-19 11:17:26 +0100

    Update Footer.js

--- src/assets/components/Footer.js
+++ src/assets/components/Footer.js
@@ -2,7 +2,8 @@
 import '../styles/footer.css';
 export default function Footer(){
   return (
-    <>
+    <className = 'containerFooter'>
+    <img src = 'https://www.iiskennedy.edu.it/pvw2/img/logo-repubblica-white.svg'/>
     </>
   );
 }
