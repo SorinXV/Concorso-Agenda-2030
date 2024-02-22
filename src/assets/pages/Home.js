679dec7461e16063cc602169c5826e867937dc87
SorinXV <sorinmartinescu06@gmail.com>
2024-02-23 00:53:38 +0100

    removed circle 3

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -67,7 +67,6 @@ export default function Home(){
     <div className='circles'>
     <div className='circle1'></div>
     <div className='circle2'></div>
-    <div className='circle3'></div>
     </div>
     </div>
   );
